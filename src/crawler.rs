3cb96820634e7f5d84f9197c5175ad89d90c8b17
M3nny <m3nny@protonmail.com>
2023-07-03 18:50:30 +0200

    Explore internal links

--- src/crawler.rs
+++ src/crawler.rs
@@ -1,7 +1,7 @@
 use scraper::{Html, Selector};
 use colored::Colorize;
 use regex::Regex;
-use std::collections::HashSet;
+use std::collections::{HashSet, VecDeque};
 
 fn find_links(domain: &str, document: &Html) -> HashSet<String> {
     let link_selector = Selector::parse("a[href]").expect("Failed to parse link selector");
@@ -12,12 +12,14 @@ fn find_links(domain: &str, document: &Html) -> HashSet<String> {
             if href.starts_with('/') {
                 let absolute_link = format!("{}{}", domain, href);
                 links.insert(absolute_link);
+            } else if !href.starts_with("http") {
+                let absolute_link = format!("{}/{}", domain, href);
+                links.insert(absolute_link);
             } else if href.starts_with(domain) {
                 links.insert(href.to_string());
             }
         }
     }
-
     links
 }
 
@@ -27,7 +29,7 @@ fn get_document(url: &str) -> Option<Html> {
         .build()
         .expect("Failed to build reqwest client");
     let response = client.get(url).send();
-    match response { // TODO: vedere se è possibile continuare l'esecuzione al posto di lanciare panic!
+    match response {
         Ok(response) => {
             if response.status().is_success() {
 
@@ -45,34 +47,58 @@ fn get_document(url: &str) -> Option<Html> {
             None
         }
     }
-
 }
 
-pub fn crawl(url: &str, word: &str) {
-    if let Some(document) = get_document(url) {
-        let links = find_links(url, &document);
+pub fn crawl(url: &str, word: &str, visited: &mut HashSet<String>) {
+    let mut found_occurence: bool;
+    let mut to_visit = VecDeque::new();
+    to_visit.push_back(url.to_string());
 
-        // selectors to be examined
-        let selectors = vec!["title", "text", "p", "h1", "h2", "h3", "h4", "h5", "h6"];
+    let pattern = format!("(?i)\\b{}\\b", word);
+    let regex = Regex::new(&pattern).expect("Failed to create regex");
 
-        let pattern = format!("(?i)\\b{}\\b", word);
-        let regex = Regex::new(&pattern).expect("Failed to create regex");
+    while let Some(current_url) = to_visit.pop_front() {
+        found_occurence = false;
+        if visited.contains(&current_url) {
+            continue;
+        }
+        visited.insert(current_url.clone());
 
-        for selector in selectors {
-            let element_selector = Selector::parse(selector).expect("Failed to parse selector");
+        if let Some(document) = get_document(&current_url) {
+            let links = find_links(&current_url, &document);
 
-            let matches: Vec<_> = document
-                .select(&element_selector)
-                .flat_map(|element| element.text())
-                .filter(|text| regex.is_match(text))
-                .collect();
+            // selectors to be examined
+            let selectors = vec!["title", "text", "p", "h1", "h2", "h3", "h4", "h5", "h6"];
 
-            // prints the contents of the tags that contain the selected word
-            for word_match in matches {
-                let colored_text = regex.replace_all(&word_match, |caps: &regex::Captures| {
-                    caps[0].red().to_string()
-                });
-                println!("- {}", colored_text);
+
+            let colored_url = current_url.green();
+            println!("{}\n--------------------", colored_url);
+            for selector in selectors {
+                let element_selector = Selector::parse(selector).expect("Failed to parse selector");
+
+                let matches: Vec<_> = document
+                    .select(&element_selector)
+                    .flat_map(|element| element.text())
+                    .filter(|text| regex.is_match(text))
+                    .collect();
+                if !matches.is_empty() {found_occurence = true};
+
+                // prints the contents of the tags that contain the selected word
+                for word_match in matches {
+                    let colored_text = regex.replace_all(&word_match, |caps: &regex::Captures| {
+                        caps[0].red().to_string()
+                    });
+                    println!("- {}", colored_text);
+                }
+            }
+            if !found_occurence {
+                println!("No occurences of {} found", word);
+            }
+            println!();
+            for link in links {
+                if !visited.contains(&link) {
+                    to_visit.push_back(link);
+                }
             }
         }
     }
