9630ba462bfeccd08d4dbeb91a6d826fe8191604
M3nny <m3nny@protonmail.com>
2023-07-04 16:56:44 +0200

    Resolve relative urls

--- src/crawler.rs
+++ src/crawler.rs
@@ -3,20 +3,49 @@ use colored::Colorize;
 use regex::Regex;
 use std::collections::{HashSet, VecDeque};
 
-fn find_links(domain: &str, document: &Html) -> HashSet<String> {
+fn resolve_relative_url(base_url: &str, relative_url: &str) -> String {
+    /*
+    * removes n subdirectories from the base_url while removing n "../" from the relative url
+    * then it attaches back the partials urls to make an entire valid url
+    */
+    let mut base_parts: Vec<&str> = base_url.split('/').collect();
+    let mut relative_parts: Vec<&str> = relative_url.split('/').collect();
+
+    // remove last element of base parts
+    base_parts.pop();
+
+    while let Some(part) = relative_parts.first() {
+        if *part == ".." {
+            base_parts.pop();
+            relative_parts.remove(0);
+        } else {
+            break;
+        }
+    }
+
+    let resolved_parts: Vec<&str> = base_parts.into_iter().chain(relative_parts.into_iter()).collect();
+    resolved_parts.join("/")
+}
+
+fn find_links(base_url: &str, document: &Html) -> HashSet<String> {
     let link_selector = Selector::parse("a[href]").expect("Failed to parse link selector");
     let mut links = HashSet::new();
 
     for element in document.select(&link_selector) {
         if let Some(href) = element.value().attr("href") {
             if href.starts_with('/') {
-                let absolute_link = format!("{}{}", domain, href);
+                let absolute_link = format!("{}{}", base_url, href);
                 links.insert(absolute_link);
-            } else if !href.starts_with("http") {
-                let absolute_link = format!("{}/{}", domain, href);
+            } else if href.starts_with("../") {
+                let absolute_link = resolve_relative_url(base_url, href);
                 links.insert(absolute_link);
-            } else if href.starts_with(domain) {
+            } else if href.starts_with("#") {
+                continue;
+            } else if href.starts_with(base_url) {
                 links.insert(href.to_string());
+            } else if !href.starts_with("http") {
+                let absolute_link = format!("{}/{}", base_url, href);
+                links.insert(absolute_link);
             }
         }
     }
@@ -38,7 +67,7 @@ fn get_document(url: &str) -> Option<Html> {
                 let document = Html::parse_document(&body);
                 Some(document)
             } else {
-                println!("Request failed with status code: {}", response.status());
+                println!("Request failed at {} with status code: {}", url.purple(), response.status());
                 None
             }
         }
@@ -50,7 +79,7 @@ fn get_document(url: &str) -> Option<Html> {
 }
 
 pub fn crawl(url: &str, word: &str, visited: &mut HashSet<String>) {
-    let mut found_occurence: bool;
+    let mut found_in_document: bool;
     let mut to_visit = VecDeque::new();
     to_visit.push_back(url.to_string());
 
@@ -58,21 +87,19 @@ pub fn crawl(url: &str, word: &str, visited: &mut HashSet<String>) {
     let regex = Regex::new(&pattern).expect("Failed to create regex");
 
     while let Some(current_url) = to_visit.pop_front() {
-        found_occurence = false;
         if visited.contains(&current_url) {
             continue;
         }
         visited.insert(current_url.clone());
 
         if let Some(document) = get_document(&current_url) {
+            found_in_document = false;
+
             let links = find_links(&current_url, &document);
 
             // selectors to be examined
             let selectors = vec!["title", "text", "p", "h1", "h2", "h3", "h4", "h5", "h6"];
 
-
-            let colored_url = current_url.green();
-            println!("{}\n--------------------", colored_url);
             for selector in selectors {
                 let element_selector = Selector::parse(selector).expect("Failed to parse selector");
 
@@ -81,20 +108,22 @@ pub fn crawl(url: &str, word: &str, visited: &mut HashSet<String>) {
                     .flat_map(|element| element.text())
                     .filter(|text| regex.is_match(text))
                     .collect();
-                if !matches.is_empty() {found_occurence = true};
 
                 // prints the contents of the tags that contain the selected word
-                for word_match in matches {
-                    let colored_text = regex.replace_all(&word_match, |caps: &regex::Captures| {
-                        caps[0].red().to_string()
-                    });
-                    println!("- {}", colored_text);
+                if !matches.is_empty() {
+                    if !found_in_document {
+                        println!("{}\n--------------------", current_url.green());
+                        found_in_document = true;
+                    }
+                    for word_match in matches {
+                        let colored_text = regex.replace_all(&word_match, |caps: &regex::Captures| {
+                            caps[0].red().to_string()
+                        });
+                        println!("- {}", colored_text);
+                    }
+                    println!();
                 }
             }
-            if !found_occurence {
-                println!("No occurences of {} found", word);
-            }
-            println!();
             for link in links {
                 if !visited.contains(&link) {
                     to_visit.push_back(link);
